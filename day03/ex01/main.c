a9dc2fa31f6a30a5dec95f2ad50f4fb19a834c54
Hoppy <mbraets@student.42.fr>
2024-04-18 17:46:32 +0200

    Add day03/ex01

---
+++ day03/ex01/main.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <uart.h>
+#include <timer.h>
+
+ISR(TIMER1_OVF_vect)
+{
+	uart_printstr("Hello World!\r\n");
+}
+
+int main(void)
+{
+	uart_init();
+
+	timer1_init(2000);
+	timer1_OVF();
+
+	while (1)
+	{
+	}
+}
