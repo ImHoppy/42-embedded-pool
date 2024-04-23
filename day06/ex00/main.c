384daa66c234e8099c8b43a38e23b191b6646a1e
Hoppy <mbraets@student.42.fr>
2024-04-23 13:35:30 +0200

    day06/ex00

---
+++ day06/ex00/main.c
@@ -0,0 +1,29 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define RED_LED (1 << PD5)
+#define GREEN_LED (1 << PD3)
+#define BLUE_LED (1 << PD6)
+
+int main()
+{
+	DDRD |= RED_LED | GREEN_LED | BLUE_LED;
+
+	PORTD = RED_LED;
+	while (1)
+	{
+		_delay_ms(1000);
+		switch (PORTD)
+		{
+		case RED_LED:
+			PORTD = BLUE_LED;
+			break;
+		case BLUE_LED:
+			PORTD = GREEN_LED;
+			break;
+		case GREEN_LED:
+			PORTD = RED_LED;
+			break;
+		}
+	}
+}
