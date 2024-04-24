13f43193d30ac53a13228e56d16e6e1a9fbe75d0
Hoppy <mbraets@student.42.fr>
2024-04-24 14:15:02 +0200

    Update day07 to use adc lib

--- day07/ex00/main.c
+++ day07/ex00/main.c
@@ -1,40 +1,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <uart.h>
+#include <adc.h>
 
 int main()
 {
-	// Set voltage reference to AVCC
-	ADMUX = (0b01 << REFS0);
-	// Select ADC0
-	ADMUX |= (0b0000 << MUX0);
-	// Set prescaler to 128
-	ADCSRA = (0b111 << ADPS0);
-	// Enable ADC
-	ADCSRA |= (1 << ADEN);
-	// // Enable ADC interrupt
-	// ADCSRA |= (1 << ADIE);
-	// sei();
-	// Start conversion
-	ADCSRA |= (1 << ADSC);
-
-	// Read in 8 bits
-	ADMUX |= (1 << ADLAR);
-
 	uart_init(UART_TX);
+
+	adc_init(ADC_NORMAL);
 
 	while (1)
 	{
-		// Wait for conversion to complete
-		while (ADCSRA & (1 << ADSC))
-			;
-		// Read only 8 bits of ADC value
-		uint8_t adc = ADCH;
-		// Send ADC value to UART
-		uart_printhex(adc);
-		uart_printstr("\n\r");
-		// Start conversion
-		ADCSRA |= (1 << ADSC);
+		// Select ADC0 Potentiometer
+		uart_printhex(adc_read8(0));
+		uart_printnl();
 		_delay_ms(20);
 	}
 }
--- day07/ex02/main.c
+++ day07/ex02/main.c
@@ -1,51 +1,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <uart.h>
-
-void print_adc_value(uint8_t channel)
-{
-	// Clear previous channel selection
-	ADMUX &= ~(0b1111 << MUX0);
-	// Select ADC channel
-	ADMUX |= (channel << MUX0);
-	// Start conversion
-	ADCSRA |= (1 << ADSC);
-	// Wait for conversion to complete
-	while (ADCSRA & (1 << ADSC))
-		;
-	// Read 10 bits of ADC value
-	uint16_t adc = ADC & 0x3FF;
-	// Send ADC value to UART
-	uart_printdec(adc);
-}
+#include <adc.h>
 
 int main()
 {
-	// Set voltage reference to AVCC
-	ADMUX = (0b01 << REFS0);
-	// Set prescaler to 128
-	ADCSRA = (0b111 << ADPS0);
-	// Enable ADC
-	ADCSRA |= (1 << ADEN);
-	// // Enable ADC interrupt
-	// ADCSRA |= (1 << ADIE);
-	// sei();
-
-	// Read in 10 bits so don't touch ADLAR
-	// ADMUX |= (1 << ADLAR);
-
 	uart_init(UART_TX);
+
+	adc_init(ADC_NORMAL);
 
 	while (1)
 	{
 		// Select ADC0 Potentiometer
-		print_adc_value(0);
+		uart_printdec(adc_read(0));
 		uart_printstr(", ");
 		// Select ADC1 LDR Sensor
-		print_adc_value(1);
+		uart_printdec(adc_read(1));
 		uart_printstr(", ");
 		// Select ADC2 Thermistor
-		print_adc_value(2);
+		uart_printdec(adc_read(2));
 		uart_printnl();
 		_delay_ms(20);
 	}
