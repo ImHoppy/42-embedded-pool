f7b823969fa4bdee9b94a32bc2d5086cae172d7e
Hoppy <mbraets@student.42.fr>
2024-04-25 14:57:36 +0200

    day08/ex02

---
+++ day08/ex02/main.c
@@ -0,0 +1,42 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <spi.h>
+
+#define RED_LED 0xff0000
+#define GREEN_LED 0x00ff00
+#define BLUE_LED 0x0000ff
+
+int main()
+{
+
+	uint8_t i = 0;
+
+	spi_init(SPI_MASTER);
+
+	while (1)
+	{
+		switch (i)
+		{
+		case 0:
+			/* code */
+			spi_set_leds(((uint32_t)0x01 << 24) | RED_LED, 0, 0);
+			break;
+		case 1:
+			/* code */
+			spi_set_leds(0, ((uint32_t)0x01 << 24) | RED_LED, 0);
+			break;
+		case 2:
+			/* code */
+			spi_set_leds(0, 0, ((uint32_t)0x01 << 24) | RED_LED);
+			break;
+		default:
+			spi_set_leds(0, 0, 0);
+			break;
+		}
+		i = (i + 1) % 4;
+
+		_delay_ms(250);
+	}
+
+	spi_end();
+}
