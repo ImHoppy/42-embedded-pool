cae2e7ebc3a5877deaeaf8ecf9f69f0e76294c84
Hoppy <mbraets@student.42.fr>
2024-04-25 14:37:59 +0200

    Turn on only red one for ex00

--- day08/ex00/main.c
+++ day08/ex00/main.c
@@ -10,8 +10,10 @@ int main()
 	spi_tx32(0);
 	// Set LED 0 to RED
 	spi_tx_led(0x1, 255, 0, 0);
-	spi_tx_led(0x1, 0, 255, 0);
-	spi_tx_led(0x1, 0, 0, 255);
+	// Turn off LED 1
+	spi_tx_led(0, 0, 0, 0);
+	// Turn off LED 2
+	spi_tx_led(0, 0, 0, 0);
 
 	// Stop transmission
 	spi_tx32(0xFFFFFFFF);
